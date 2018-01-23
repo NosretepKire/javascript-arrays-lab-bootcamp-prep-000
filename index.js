b0c4bee6a7550a9338444a6b74d9d08debe1cfbc
Erik Peterson <erikeh1@gmail.com>
2018-01-23 21:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(name){
-  name.push();
+function destructivelyAppendKitten(name,element){
+  name.push(element);
   return name;
 }
