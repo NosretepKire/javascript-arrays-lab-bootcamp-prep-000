0d36966f420d584d42f133170b42ee487a87b2ec
Erik Peterson <erikeh1@gmail.com>
2018-01-23 21:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten (name){
   kittens.push(name);
-  return kittens;
+    return kittens;
 }
 function destructivelyPrependKitten(name){
    kittens.unshift(name);
-  return kittens;
+    return kittens;
 }
 function destructivelyRemoveLastKitten(){
   kittens.pop()
-  return kittens;
+    return kittens;
 }
+function 
